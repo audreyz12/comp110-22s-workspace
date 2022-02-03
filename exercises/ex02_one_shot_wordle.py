9ff7b0afb4d6e8227b1f9350080d2c9cdd2a291c
Audrey Zhou <azhou@unc.edu>
2022-02-03 11:30:48 -0500

    Completed Ex02

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -2,39 +2,49 @@
 
 __author__ = "730480148"
 
+# declare variables
 secret: str = "python"
 length: int = len(secret)
-
+i: int = 0
+boxes: str = ""
 guess: str = input(f"What is your {length}-letter guess? ")
 
+# if the guess is not the correct length, ask user to try again
 while len(guess) != length:
     guess = input(f"That was not {length} letters! Try again: ")
 
-i: int = 0
-boxes: str = ""
-
 
 while i < len(secret):
+    # if the character guessed is correct and in the right position, add a green box
     if guess[i] == secret[i]:
-        boxes = boxes + "\U0001F7E9"
+        boxes += "\U0001F7E9"
     else:
+        # if the character guessed is not correct and/or in the right position, 
+        # check to see if the character guessed matches up with any character in the
+        # secret word
         exists: bool = False
         alt: int = 0
         while alt < len(secret):
+            # if the character guessed matches up with some character in the secret word
+            # but is not in the correct position, notify the program that a yellow box should
+            # be added
             if guess[i] == secret[alt]:
                 exists = True
                 alt = len(secret)
             else:
-                alt = alt + 1
+                alt += 1
+        # add a yellow box if the character guessed exists in the secret word, but is not
+        # in the correct position
         if exists:
-            boxes = boxes + "\U0001F7E8"
+            boxes += "\U0001F7E8"
+        # if the character guessed isn't in the secret word at all, add a white box
         else:
-            boxes = boxes + "\U00002B1C"
+            boxes += "\U00002B1C"
         alt = len(secret)    
-    i = i + 1
-
+    i += 1
 print(boxes)
 
+# tell the user if the guess is correct
 if guess == secret:
     print("Woo! You got it!")
 else:
