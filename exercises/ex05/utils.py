3195e0ac8417e66de8f24caaf907e3dd37239079
Audrey Zhou <azhou@unc.edu>
2022-02-21 23:28:05 -0500

    Progress with lessons and ex05.

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -4,21 +4,43 @@ __author__ = "730480148"
 
 
 def only_evens(xs: list[int]) -> list[int]:
-    """Returns a list of integers containing only the even elements of the input parameter."""
+    """Returns a list of integers containing only the even elements of the input list."""
     evens: list[int] = list()
+    # for each item in the list, check to see if there is a remainder when divided by two
     for x in xs:
-        if xs[i] % 2 == 0:
-            evens.append(xs[i])
+        # no remainder means the item is even and should be added to the list that will be returned
+        if x % 2 == 0:
+            evens.append(x)
     return evens
 
 
 def sub(xs: list[int], start: int, end: int) -> list[int]:
-    """Returns a list that is a subset of the given list, between the specified indexes."""
+    """Returns a list that is a subset of the given list, between the specified indexes, not inclusive at the end index."""
     subset: list[int] = list()
-    if start < end - 1:
-        while start < end - 1:
-            subset.append(xs[start])
-            start += 1
-    elif start == end:
-        subset.append(xs[start])
+    # set starting index to zero if it is negative
+    if start < 0:
+        start = 0
+    # set ending index to the length of the list if it is greater than that value
+    if end > len(xs):
+        end = len(xs)
+    # return an empty list if given unrealistic parameters
+    if len(xs) == 0 or start > len(xs) or end <= 0:
         return subset
+    # while the starting index is less than the ending index, add the element at that index
+    # to the list to be returned
+    while start < end:
+        subset.append(xs[start])
+        start += 1
+    return subset
+
+
+def concat(xs: list[int], ys: list[int]) -> list[int]:
+    """Returns a list containing all of the elements of the first list followed by all of the elements of the second list."""
+    combined: list[int] = list()
+    # add each element in the first list to the list to be returned
+    for x in xs:
+        combined.append(x)
+    # then add each element in the second list to the list to be returned, after the first list
+    for y in ys:
+        combined.append(y)
+    return combined
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -2,14 +2,61 @@
 
 __author__ = "730480148"
 
-from exercises.ex05.utils import only_evens
+from utils import only_evens, sub, concat
 
 
-def test_only_evens() -> None:
+def test_only_evens_normal() -> None:
+    """Tests only_evens for a typical usage of the function."""
     xs: list[int] = [1, 2, 5]
     assert only_evens(xs) == [2] 
 
 
 def test_only_evens_empty() -> None:
+    """Tests only_evens when given an empty list."""
     xs: list[int] = []
     assert only_evens(xs) == []
+
+
+def test_only_evens_odds() -> None:
+    """Tests only_evens when given a list with no even elements."""
+    xs: list[int] = [1, 3, 3, 5]
+    assert only_evens(xs) == []
+
+
+def test_sub_normal() -> None:
+    """Tests sub for a typical usage of the function."""
+    xs: list[int] = [10, 20, 30, 40]
+    assert sub(xs, 1, 3) == [20, 30]
+
+
+def test_sub_empty() -> None:
+    """Tests sub when given an empty list."""
+    xs: list[int] = []
+    assert sub(xs, -1, 1) == []
+
+
+def test_sub_out_of_bounds() -> None:
+    """Tests sub when given indexes that are out of the range of the list."""
+    xs: list[int] = [2, 4, 6, 23]
+    assert sub(xs, 10, 5) == []
+
+
+def test_concat_normal() -> None:
+    """Tests concat for a typical usage of the function."""
+    xs: list[int] = [1, 2, 3, 4]
+    ys: list[int] = [3, 2, 1]
+    assert concat(xs, ys) == [1, 2, 3, 4, 3, 2, 1]
+
+
+def test_concat_one_empty() -> None:
+    """Tests concat when one list is empty."""
+    xs: list[int] = [1, 2, 3, 4]
+    ys: list[int] = []
+    assert concat(xs, ys) == [1, 2, 3, 4]
+
+
+def test_concat_both_empty() -> None:
+    """Tests concat when both lists are empty."""
+    xs: list[int] = []
+    ys: list[int] = []
+    assert concat(xs, ys) == []
