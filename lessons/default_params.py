388f1eccbc736625ddd46423355352197d99a6c0
Audrey Zhou <azhou@unc.edu>
2022-03-31 11:38:38 -0400

    Learning.

---
+++ lessons/default_params.py
@@ -0,0 +1,12 @@
+"""Examples of default parameters."""
+
+
+# once you add a default parameter, everything after that has to be default
+def add(x: int, y: int = 0, z: int = 0) -> int:
+    """Example of a default parameter where y and z are 0 by default."""
+    return x + y + z
+
+
+print(add(1))
+print(add(1, 2))
+print(add(1, 2, 3))
